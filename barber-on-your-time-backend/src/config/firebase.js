b773e7b40b507d68f97c5ea4ac6fd41e8c75202b
Obida <obidamalla@gmail.com>
2026-09-06 12:00:39 +0300

    Read Firebase credentials from env variable

--- barber-on-your-time-backend/src/config/firebase.js
+++ barber-on-your-time-backend/src/config/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp, cert } from "firebase-admin/app";
+/*import { initializeApp, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
@@ -8,6 +8,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const serviceAccount = JSON.parse(
   readFileSync(join(__dirname, "../../firebase-service-account.json"), "utf-8")
 );
+
+initializeApp({ credential: cert(serviceAccount) });
+
+const messaging = getMessaging();
+export default messaging;*/
+import { initializeApp, cert } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({ credential: cert(serviceAccount) });
 
